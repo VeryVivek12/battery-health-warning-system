1d71553110b4418876f943aba3b77254e7813d91
Vivek Topiya <vivektopiya12@gmail.com>
2024-10-24 12:27:25 +0530

    Ktable added for logging connected cars

--- health-data-processor/src/main/java/com/valtech/poc/hdp/process/BatteryDataStreamProcessor.java
+++ health-data-processor/src/main/java/com/valtech/poc/hdp/process/BatteryDataStreamProcessor.java
@@ -4,8 +4,10 @@ import com.valtech.poc.core.dto.BatteryHealthData;
 import com.valtech.poc.core.dto.CellHealthData;
 import com.valtech.poc.core.dto.ProcessedBatteryHealthData;
 import com.valtech.poc.hdp.serde.ProcessedBatteryHealthDataSerde;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,16 @@ import org.springframework.stereotype.Component;
 import static com.valtech.poc.core.Constants.*;
 
 @Component
+@Slf4j
 public class BatteryDataStreamProcessor {
 
     @Value(value = "${spring.kafka.outgoing-topic}")
     private String pocBatteryDataProcessed;
 
     public void process(KStream<String, BatteryHealthData> stream) {
+
+        final KTable<String, Long> dataCountsForCar = stream.groupByKey().count();
+        dataCountsForCar.toStream().foreach((key, value) -> log.info("{} is connected to Kafka cluster", key));
 
         stream.mapValues((carId, batteryHealthData) -> {
             // get average cell health
